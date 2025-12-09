735673cccfe8f4f5d1c9048eecab68b981da1c53
Mihaela Radian <mihaela.radian@multiversx.com>
2025-12-09 15:53:43 +0200

    MX- 17385 Added entry counter

---
+++ multiversx_logs_counter/counter_checker.py
@@ -0,0 +1,37 @@
+from argparse import Namespace
+from typing import Any
+from multiversx_logs_parser_tools.node_logs_checker import NodeLogsChecker
+
+from .counter_parser import CounterData, CounterParser
+
+
+class CounterChecker(NodeLogsChecker):
+
+    def __init__(self, parser_cls: type[CounterParser], args: Namespace):
+        super().__init__(parser_cls, args)
+
+    def initialize_checker(self, args):
+        self.parsed = CounterData()
+        return super().initialize_checker(args)
+
+    def process_parsed_result(self):
+        self.parsed.add_counted_messages(self.parser.counters)
+        self.parser.initialize_checker()
+
+    def post_process_node_logs(self):
+        # Implement post-processing logic here
+        self.write_node_json(path=f'./Reports/{self.run_name}/LogEntryCounter')
+
+    def create_json_for_node(self) -> dict[str, Any]:
+        for log_level, messages in self.parsed.counter_dictionary.items():
+            messages_sorted = dict(sorted(messages.items(), key=lambda item: item[1], reverse=True))
+            self.parsed.counter_dictionary[log_level] = messages_sorted
+        return {
+            "node_name": self.node_name,
+            "run_name": self.run_name,
+            "counter": self.parsed.counter_dictionary
+        }
+
+    def reset_node(self, args: Namespace):
+        super().reset_node(args)
+        self.parsed = CounterData()
---
+++ multiversx_logs_counter/counter_parser.py
@@ -0,0 +1,38 @@
+from re import Pattern
+from typing import Any
+from multiversx_logs_parser_tools.aho_corasik_parser import AhoCorasickParser
+
+from .counter_structures import CounterData
+
+
+class CounterParser(AhoCorasickParser):
+    def __init__(self):
+        super().__init__()
+        self.counters = CounterData()
+
+    def get_patterns(self) -> list[tuple[Pattern[str], int]]:
+        patterns = []
+        patterns.append(('INFO', 0))
+        patterns.append(('TRACE', 1))
+        patterns.append(('DEBUG', 2))
+        patterns.append(('WARN', 3))
+        patterns.append(('ERROR', 4))
+
+        return patterns
+
+    def initialize_checker(self) -> None:
+        self.counters = CounterData()
+
+    def process_match(self, line: str, end_index: int, pattern_idx: int, args: dict[str, str]) -> dict[str, Any]:
+        parsed = super().process_match(line, end_index, pattern_idx, args)
+        return parsed
+
+    def process_parsed_entry(self, parsed_entry: dict[str, Any], args: dict[str, str]) -> None:
+        level = parsed_entry.get('logger_level', 'UNKNOWN')
+        message = parsed_entry.get('message', '')
+
+        if message:
+            self.counters.add_message(level, message)
+
+    def should_parse_line(self, pattern: Pattern[str]) -> bool:
+        return True
